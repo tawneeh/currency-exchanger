737b6c28872630549613c53018b84922fbebf1ef
Tawnee Harris <tawneeh@icloud.com>
2020-11-20 16:05:42 -0800

    switch gears to XHR object with promise

--- src/services/currency-service.js
+++ src/services/currency-service.js
@@ -1,13 +1,18 @@
 export default class CurrencyService {
-  static async getExchange() {
-    try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
-      if (!response.ok) {
-        throw Error(response.statusText);
+  static getExchange() {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const link = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
       }
-      return response.json();
-    } catch(error) {
-      return error.message;
-    }
+      request.open("GET", link, true);
+      request.send();
+    });
   }
 }
