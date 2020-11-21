3467090c7363595beee2c66a5461ee9ab3a15666
Tawnee Harris <tawneeh@icloud.com>
2020-11-20 16:27:43 -0800

    add console logs. I'm not sre what is missing

--- src/main.js
+++ src/main.js
@@ -10,11 +10,14 @@ function clearFields() {
 
 $(document).ready(function() {
   $('#exchange').click(function() {
+    console.log("Click");
     clearFields();
     let promise = CurrencyService.getExchange();
     promise.then(function(response) {
       const data = JSON.parse(response);
+      console.log("JSON data");
       $('.show-exchange').text(`The currency exchange is ${data.conversion_rates}`);
+      console.log("Show api response");
     }, function(error) {
       $('.show-errors').text(`There was an error with your request: ${error}`);
     });
