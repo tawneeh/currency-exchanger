4d28ed3b4117d1297b16be24a6bf7b9d518e9a2e
Tawnee Harris <tawneeh@icloud.com>
2020-11-21 17:13:50 -0800

    add missing DotEnv plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/main.js',
@@ -13,6 +14,7 @@ module.exports = {
     contentBase: './dist'    
   },
   plugins: [
+    new Dotenv(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Currency Exchanger',
