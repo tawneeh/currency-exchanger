8728df68f2a948020de73effd3410d29d7a0d7b1
Tawnee Harris <tawneeh@icloud.com>
2020-12-05 17:45:22 -0800

    add 'this currency does not exist' error

--- src/main.js
+++ src/main.js
@@ -19,13 +19,13 @@ $(document).ready(function() {
         if (response instanceof Error) {
           throw Error(`There was an error - ${response.message}`);
         } else if (isNaN(response.conversion_rates[currency])) {
-          throw Error(`There was an error: The currency does not exist.`);
+          throw Error(`There was an error: This currency does not exist.`);
         } else {
-        const data = response; 
-        const rate = data.conversion_rates[currency];
-        const num = rate * USD;
-        const amount = num.toFixed(2);
-        $('.show-exchange').text(`The currency exchange rate is $${rate} and the amount is ¤${amount}`);
+          const data = response; 
+          const rate = data.conversion_rates[currency];
+          const num = rate * USD;
+          const amount = num.toFixed(2);
+          $('.show-exchange').text(`The currency exchange rate is $${rate} and the amount is ¤${amount}`);
         }
       })
       .catch(function(error) {
