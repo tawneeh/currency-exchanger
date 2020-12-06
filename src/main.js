d40846f2470d1a67a6a0afd3db53cc518f8a05e2
Tawnee Harris <tawneeh@icloud.com>
2020-12-05 17:35:40 -0800

    adjust currency error handling

--- src/main.js
+++ src/main.js
@@ -16,14 +16,17 @@ $(document).ready(function() {
 
     CurrencyService.getExchange()
       .then(function(response) {
-        if (response instanceof Error || currency !== currency) {
+        if (response instanceof Error) {
           throw Error(`There was an error - ${response.message}`);
-        }
+        } else if (isNaN(response.conversion_rates[currency])) {
+          throw Error(`There was an error: The currency does not exist.`);
+        } else {
         const data = response; 
         const rate = data.conversion_rates[currency];
         const num = rate * USD;
         const amount = num.toFixed(2);
         $('.show-exchange').text(`The currency exchange rate is $${rate} and the amount is ¤${amount}`);
+        }
       })
       .catch(function(error) {
         displayErrors(error.message);
